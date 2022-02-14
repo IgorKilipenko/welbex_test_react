ee26d1d0c372f6d0ba37e593c35f421dd2278c05
IgorKilipenko <igorkili@mail.ru>
2022-02-14 17:32:37 +0700

    sfix

--- components/theme/theme.js
+++ components/theme/theme.js
@@ -1,7 +1,7 @@
 const theme = () => {
     return {
         bgColor: (opacity=1) => `rgba(17, 87, 64, ${opacity})`,
-        bgColorLight : (opacity=1) => `rgba(155, 227, 191, ${opacity}`,
+        bgColorLight : (opacity=1) => `rgba(155, 227, 191, ${opacity})`,
         textColor: (opacity=1) => `rgba(255, 255, 255, ${opacity})`,
     }
 }
