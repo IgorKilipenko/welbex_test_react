6b4e20c1f71516f04e8c5d19ea5d614b320329b8
IgorKilipenko <igorkili@mail.ru>
2022-02-14 21:25:46 +0700

    add MenuOverlay

--- components/view/index.js
+++ components/view/index.js
@@ -1,5 +1,6 @@
 import Layout from './layout'
 import AppBar from './app_bar'
 import { WrappedImage } from './image'
+import MenuOverlay from './main_menu'
 
-export { Layout, AppBar, WrappedImage }
+export { Layout, AppBar, MenuOverlay, WrappedImage }
