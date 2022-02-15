d01314ac3da8718f70f4a706c8d3198a2446224f
IgorKilipenko <igorkili@mail.ru>
2022-02-16 05:55:15 +0700

    fix next/prev page and mobile hover

--- components/view/home/todo_list.js
+++ components/view/home/todo_list.js
@@ -9,16 +9,29 @@ import Pagination from '../pagination'
 const stylesFactory = memoStylesFactory((theme) => {
     const { absoluteCenter } = styleUtils
     const { textColorDark, bp } = theme
+    const fontSize = 5
+    const horizontalLine = {
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: 1,
+        backgroundColor: textColorDark(0.25),
+    }
     return {
         container: {
             absoluteCenter,
             color: textColorDark(),
-            fontSize: '5rem',
+            fontSize: `${fontSize / 2}rem`,
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'flex-start',
             alignItems: 'center',
             width: '100%',
+            [bp.median]: {
+                fontSize: `${fontSize}rem`,
+            },
         },
         scrollContainer: {
             position: 'relative',
@@ -27,24 +40,32 @@ const stylesFactory = memoStylesFactory((theme) => {
             overflow: 'hidden',
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
+            alignItems: 'stretch',
         },
-        footer: {},
+        item: {
+            position: 'relative',
+            margin: '0 5%',
+            [bp.median]: {
+                margin: '0 20%',
+            },
+            '&::before': {
+                ...horizontalLine,
+            },
+            '& > div': {
+                padding: '1rem',
+                [bp.median]: {
+                    padding: '2rem',
+                },
+            },
+        },
         paginationBar: {
             position: 'relative',
-            //width: '100%',
             margin: '2rem auto',
             paddingTop: '1rem',
             display: 'flex',
             justifyContent: 'center',
             '&::after': {
-                content: '""',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: 1,
-                backgroundColor: textColorDark(0.25),
+                ...horizontalLine,
             },
         },
     }
@@ -55,30 +76,41 @@ const TodoList = () => {
     const styles = stylesFactory(theme)
     const [currentPage, setCurrentPage] = useState(1)
     const todos = useTodoState()
-    const pageSize = 20
+    const pageSize = 5
     const data = useRef([])
+    const scrollRef = useRef(null)
 
-
-    const prepreData = useCallback(()=> {
+    const prepreData = useCallback(() => {
         return !todos.error && !todos.loading && todos.entities
-        ? Object.entries(todos.entities).map(([k, v]) => {
-              return v
-          })
-        : []
-    },[todos])
+            ? Object.entries(todos.entities).map(([k, v]) => {
+                  return { id: k, ...v }
+              })
+            : []
+    }, [todos])
 
     const currentTableData = useMemo(() => {
         const firstPageIndex = (currentPage - 1) * pageSize
         const lastPageIndex = firstPageIndex + pageSize
         data.current = prepreData()
         return data.current.slice(firstPageIndex, lastPageIndex)
-        
     }, [currentPage, prepreData])
 
+    const handlePageChange = (page) => {
+        setCurrentPage(prevPage => {
+            if (prevPage !== page) {
+                if (scrollRef.current) {
+                    scrollRef.current.scrollTop(0)
+                }
+            }
+            return page
+        })
+
+    }
 
     return (
         <div css={styles.container}>
             <Scrollbars
+                ref={scrollRef}
                 universal
                 autoHide
                 autoHideTimeout={1000}
@@ -86,8 +118,18 @@ const TodoList = () => {
                 css={styles.scrollContainer}>
                 <ul css={styles.list}>
                     {currentTableData.map((item, i) => {
-                        const key = `${nanoid()}_${i}`
-                        return <li key={key}><div>{Object.values(item).join(', ')}</div></li>
+                        
+                        const {id:todoId, ...values} = item
+                        const key = `${todoId}`
+                        return (
+                            <li key={key} todo-id={todoId} css={styles.item}>
+                                <div>
+                                    {Object.entries(values).map(([k, v], i) => {
+                                        return (<p key={i}>{`${k} : ${v}`}</p>)
+                                    })}
+                                </div>
+                            </li>
+                        )
                     })}
                 </ul>
             </Scrollbars>
@@ -97,7 +139,7 @@ const TodoList = () => {
                 totalCount={data.current.length}
                 pageSize={pageSize}
                 siblingCount={1}
-                onPageChange={(page) => setCurrentPage(page)}
+                onPageChange={(page) => handlePageChange(page)}
             />
         </div>
     )
