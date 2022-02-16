33b417f550b4b39f3568297d55d395b0e5458719
IgorKilipenko <igorkili@mail.ru>
2022-02-17 03:30:57 +0700

    nfix styles

--- components/view/main_menu/nav/nav_container.js
+++ components/view/main_menu/nav/nav_container.js
@@ -1,5 +1,34 @@
-const NavContainer = ({children}) => {
-    return <nav className="flex flex-wrap w-full items-end justify-between pb-40 m:pb-60" {...{children}}/>
+import { useTheme } from '@emotion/react'
+import { memoStylesFactory, styleUtils } from '@Styles'
+
+const stylesFactory = memoStylesFactory((theme) => {
+    const { fullSize } = styleUtils
+    const { bp } = theme
+    return {
+        container: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'flex-end',
+            justifyContent: 'space-between',
+            paddingBottom: '4rem',
+            width: '100%',
+            [bp.median]: {
+                paddingBottom: '6rem',
+            },
+        },
+    }
+})
+
+const NavContainer = ({ children }) => {
+    const theme = useTheme()
+    const styles = stylesFactory(theme)
+    return (
+        <nav
+            css={styles.container}
+            //className="flex flex-wrap w-full items-end justify-between pb-40 m:pb-60"
+            {...{ children }}
+        />
+    )
 }
 
 export default NavContainer
