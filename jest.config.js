b72dc51549e9154c539572c6e123e0c607816a64
IgorKilipenko <igorkili@mail.ru>
2022-02-16 21:10:58 +0700

    add paths

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,20 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    '^@Components/(.*)$': '<rootDir>/components/$1',
+    '^@Components$': '<rootDir>/components',
+    '^@Styles/(.*)$': '<rootDir>/styles/$1',
+    '^@Styles$': '<rootDir>/styles',
+    '^@Store$': '<rootDir>/components/store',
+    '^@Store/(.*)$': '<rootDir>/components/store/$1',
+    '^@Hooks$': '<rootDir>/hooks',
+    '^@Utils$': '<rootDir>/utils',
+    '^@Pages/(.*)$': '<rootDir>/pages/$1',
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
