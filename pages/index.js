8035652e1ee336b525aa0874bb39428546af3a37
IgorKilipenko <igorkili@mail.ru>
2022-02-17 10:52:55 +0700

    clean

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,8 @@ import { useTheme } from '@emotion/react'
 import { memoStylesFactory, styleUtils } from '@Styles'
 import Head from 'next/head'
 import HomeComponent from '@Components/view/home'
-import { updateTodoList } from '@Store/'
-import { useCallback, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
 
 const stylesFactory = memoStylesFactory((theme) => {
-    const { bp } = theme
     return {
         container: {
             position: 'relative',
@@ -22,25 +18,7 @@ const stylesFactory = memoStylesFactory((theme) => {
 const HomePage = (/*props*/) => {
     const theme = useTheme()
     const styles = stylesFactory(theme)
-    const dispatch = useDispatch()
 
-    const update = useCallback(async () => {
-        const resultAction = await dispatch(updateTodoList({}))
-        if (updateTodoList.fulfilled.match(resultAction)) {
-            console.debug('success loaded todos')
-        } else {
-            if (resultAction.payload) {
-                console.error(resultAction.payload.field_errors)
-            } else {
-                console.error('error', `Update failed: ${resultAction.error}`)
-            }
-        }
-    },[dispatch])
-
-    useEffect(() => {
-        update().catch(console.error)
-    },[update])
-    
     return (
         <>
             <Head>
