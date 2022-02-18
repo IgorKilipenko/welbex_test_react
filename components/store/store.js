4ad5ef47e577d2863ed583e94c2af89783089cf6
IgorKilipenko <igorkili@mail.ru>
2022-02-18 09:53:53 +0700

    config logger

--- components/store/store.js
+++ components/store/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { slicies } from './reducers'
-import logger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 
+const logger = createLogger({
+    predicate: (getState, action) =>
+        action.type !== 'components/setMainComponentMousePosition',
+    collapsed: (getState, action, logEntry) => !logEntry.error,
+})
 
 const themeSlice = slicies.theme
 const componentsSlice = slicies.components
@@ -13,20 +18,25 @@ const actions = {
     todos: { ...todoSlice.actions },
 }
 
-const configureAppStore = ({ theme, components, todos}) => {
+const configureAppStore = ({ theme, components, todos }) => {
     const reducer = {
         theme: themeSlice.reducer,
         components: componentsSlice.reducer,
-        todos: todoSlice.reducer
+        todos: todoSlice.reducer,
     }
     const store = configureStore({
         reducer,
         preloadedState: {
             theme: { ...themeSlice.getInitialState(), ...theme },
             components: { ...componentsSlice.getInitialState(), ...components },
-            todos: { ...todoSlice.getInitialState(), ...todos}
+            todos: { ...todoSlice.getInitialState(), ...todos },
         },
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+        middleware: (getDefaultMiddleware) => {
+            if (process.env.NODE_ENV === `development`) {
+                return getDefaultMiddleware().concat(logger)
+            }
+            return getDefaultMiddleware()
+        },
         devTools: process.env.NODE_ENV !== 'production',
     })
     return store
